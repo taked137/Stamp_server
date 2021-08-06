6137c2db2c78261e3c8862ced8fa97cd7c05b854
EC2 Default User <ec2-user@ip-172-31-32-97.ap-northeast-1.compute.internal>
2021-08-06 05:55:43 +0000

    [add] return quiz image

--- server.go
+++ server.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+    "time"
+    "math/rand"
     "net/http"
 
     "github.com/labstack/echo/v4"
+)
+
+type (
+    beaconRequest struct {
+        Quiz int `json:"quiz"`
+        Beacon []int `json:"beacon"`
+    }
+    beaconResponse struct {
+        ID int `json:"id"`
+        Quiz int `json:"quiz"`
+        URL string `json:"url"`
+    }
 )
 
 func main() {
@@ -14,6 +28,27 @@ func main() {
     e.GET("/:user", func(c echo.Context) error {
         user := c.Param("user")
         return c.String(http.StatusOK, "Hello, World!" + user)
+    })
+    e.POST("/stamp/image", func(c echo.Context) error {
+        request := new(beaconRequest)
+        if err := c.Bind(request); err != nil {
+            return err
+        }
+
+        // num := 0
+        // for _,v := range request.Beacon {
+        //     num += v
+        // }
+
+        rand.Seed(time.Now().UnixNano())
+        num := rand.Intn(3)
+
+        response := beaconResponse {
+            ID: num,
+            Quiz: request.Quiz,
+            URL: "https://1.bp.blogspot.com/-3XfMA0UhT70/XlyfrsiokjI/AAAAAAABXn8/j_CLCc73TTEi-PCK19hnUwY3D-pJgmjvQCNcBGAsYHQ/s1600/drink_beer_yukata_man.png",
+        }
+        return c.JSON(http.StatusOK, response)
     })
     e.Logger.Fatal(e.Start(":1323"))
 }

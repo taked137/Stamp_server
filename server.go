aefaca573322c9687dd8fcef6529ba092836005e
taked1664 <taked1664@gmail.com>
2021-08-12 17:09:09 +0000

    [add] quiz image

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
     "time"
     "os"
+    "strconv"
 
     "math/rand"
     "net/http"
@@ -25,6 +26,13 @@ type (
     userResponse struct {
         UUID string `json:"uuid"`
     }
+
+    imageRequest struct {
+        Quiz int `json:"quiz"`
+    }
+    imageResponse struct {
+        URL string `json:"url"`
+    }
     
     beaconRequest struct {
         Quiz int `json:"quiz"`
@@ -37,15 +45,36 @@ type (
     }
 )
 
+const (
+    BASE_URL string = "http://13.113.250.233:1323"
+)
+
 func main() {
     e := echo.New()
     e.GET("/", func(c echo.Context) error {
         return c.String(http.StatusOK, "Hello, World!")
     })
-    e.GET("/:user", func(c echo.Context) error {
-        user := c.Param("user")
-        return c.String(http.StatusOK, "Hello, World!" + user)
-    })
+    e.GET("/stamp/quiz/:num", func(c echo.Context) error {
+        num, _ := strconv.Atoi(c.Param("num"))
+        switch num {
+            case 1:
+                return c.File("./images/quiz1.png")
+            case 2:
+                return c.File("./images/quiz2.png")
+            case 3:
+                return c.File("./images/quiz3.png")
+            case 4:
+                return c.File("./images/quiz4.png")
+            case 5:
+                return c.File("./images/quiz5.png")
+            case 6:
+                return c.File("./images/quiz6.png")
+            default:
+                code := http.StatusNotFound
+                msg := http.StatusText(code)
+                return c.JSON(code, msg)
+        }
+    })  
     e.POST("/user/create", func (c echo.Context) error {
         request := new(userRequest)
         if err := c.Bind(request); err != nil {
@@ -85,26 +114,33 @@ func main() {
         }
         return c.JSON(http.StatusOK, response)
     })
-    e.POST("/stamp/image", func(c echo.Context) error {
+    e.GET("/stamp/image/:num", func(c echo.Context) error {
+        num := c.Param("num")
+        response := imageResponse {
+            URL : BASE_URL + "/stamp/quiz/" + num,
+        }
+        return c.JSON(http.StatusOK, response)
+    })
+    e.POST("/stamp/beacon", func(c echo.Context) error {
         request := new(beaconRequest)
         if err := c.Bind(request); err != nil {
             return err
         }
 
-        // num := 0
-        // for _,v := range request.Beacon {
-        //     num += v
-        // }
-
         rand.Seed(time.Now().UnixNano())
-        num := rand.Intn(3)
+        num := rand.Intn(6)
+	    if(1 < num) {
+		    num = 2
+	    }
 
         response := beaconResponse {
             ID : num,
             Quiz : request.Quiz,
-            URL : "https://1.bp.blogspot.com/-3XfMA0UhT70/XlyfrsiokjI/AAAAAAABXn8/j_CLCc73TTEi-PCK19hnUwY3D-pJgmjvQCNcBGAsYHQ/s1600/drink_beer_yukata_man.png",
+            //URL : "https://1.bp.blogspot.com/-3XfMA0UhT70/XlyfrsiokjI/AAAAAAABXn8/j_CLCc73TTEi-PCK19hnUwY3D-pJgmjvQCNcBGAsYHQ/s1600/drink_beer_yukata_man.png",
+            URL : BASE_URL + "/stamp/quiz/" + strconv.Itoa(request.Quiz),
         }
         return c.JSON(http.StatusOK, response)
     })
+
     e.Logger.Fatal(e.Start(":1323"))
 }
